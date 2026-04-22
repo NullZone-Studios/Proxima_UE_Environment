f67e981239335c718a58ac357070b58ece3da31a
TheNerdySlayer <180648394+TheNerdySlayer@users.noreply.github.com>
2026-04-22 16:15:09 +0200

    Refactor player stats and add progression properties
    Grouped base stats under "Player Stats|Base Stats" and introduced new progression properties (XP, NextLevelXP, Level, LevelUpMultiplier) under "Player Stats|Progression" in PlayerCharacter.h for improved organization and extensibility.

--- Source/UE_Environment/Public/PlayerCharacter.h
+++ Source/UE_Environment/Public/PlayerCharacter.h
@@ -19,18 +19,26 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Player Stats")
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Player Stats|Base Stats")
 	float Health;
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Player Stats")
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Player Stats|Base Stats")
 	float MaxHealth;
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Player Stats")
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Player Stats|Base Stats")
 	float Damage;
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Player Stats")
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Player Stats|Base Stats")
 	float Defense;
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Player Stats")
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Player Stats|Base Stats")
 	float AttackSpeed;
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Player Stats")
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Player Stats|Base Stats")
 	float MovementSpeed;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Player Stats|Progression")
+	float XP;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Player Stats|Progression")
+	float NextLevelXP;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Player Stats|Progression")
+	int32 Level;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Player Stats|Progression")
+	float LevelUpMultiplier;
 
 public:	
 	// Called every frame
